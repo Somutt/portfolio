5a649337797d1498c79617a985fd39cb1b457cfc
Samuel Moura Alves <samuelmalves1@gmail.com>
2023-10-24 07:55:01 -0300

    initial config

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,5 @@
+import { Title } from '../../components/Title';
+
+export const Home = () => {
+  return <Title title='Hello World!' />;
+};
---
+++ styles/theme.ts
@@ -0,0 +1,22 @@
+export const theme = {
+  colors: {
+    white: '#f8f8f8',
+    dark: '#1f1f1f',
+  },
+  font: {
+    family: {
+      primary: "'Roboto', sans-serif",
+      secondary: "'Helvetica', sans-serif",
+    },
+    sizes: {
+      small: '1.2rem',
+      default: '1.6rem',
+      medium: '2.4rem',
+      big: '3.2rem',
+      large: '4.0rem',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+  },
+} as const;
