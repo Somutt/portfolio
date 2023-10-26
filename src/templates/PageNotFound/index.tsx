d9d8652d019d94c8d179f384a1d478c1bd9a3672
Samuel Moura Alves <samuelmalves1@gmail.com>
2023-10-26 08:23:48 -0300

    PageNotFound updated

--- src/templates/PageNotFound/index.tsx
+++ src/templates/PageNotFound/index.tsx
@@ -5,8 +5,8 @@ export const PageNotFound = () => {
   return (
     <Styled.Container>
       <h2>
-        A página que procura não foi encontrada |{' '}
-        <Link href='/'>Clique aqui para retornar</Link>
+        The page you wanted was not found |{' '}
+        <Link href='/'>Click here to go back!</Link>
       </h2>
     </Styled.Container>
   );
